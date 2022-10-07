747cf9d585f280f9c784f2a99b8555c94342e9d9
Anthony Gomez <gomez9162@gmail.com>
2022-10-07 11:09:03 -0500

    Linked List

--- Linked-List/Node.py
+++ Linked-List/Node.py
@@ -18,10 +18,10 @@ class LinkedList:
         if self.head == None: # Check is the lest is empty
             self.head = new_node # This will assign the new node to the head of the linked list
         else:
-            new_node.next = self.head # This will allow for the node to point to the new node
-            self.head = new_node # This points to None
+            new_node.next = self.head # This will point to the current head of the list(The New node to equal to the head)
+            self.head = new_node # This will move the head to the new node of the list
 
-    def append(self, data):
+    def appendEnd(self, data):
         new_node = Node(data) # Create a new node
         if self.head == None: # Check if head of the list doesnt have a components or points to anything.
             self.head = new_node # Then set the head pointer to the new node created.
@@ -32,6 +32,11 @@ class LinkedList:
             last_node.next = new_node # Once the head is pointing to None, then insert the node
 
 llist = LinkedList()
-llist.append('A')
-llist.append('B')
+llist.appendEnd('A')
+llist.appendEnd('B')
+llist.appendEnd('C')
+llist.appendEnd('D')
+
+llist.insertAtBeginning("E")
+
 llist.printList()
